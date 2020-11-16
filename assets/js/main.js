884052d5f0a5db80e10d4f19e3b0f3e55ec3c62f
Alessandro <internicola.alessandro@gmail.com>
2020-11-16 16:35:47 +0100

    Bottone genera 90% completo
    Aggiunto il file css, sistemato un flex generico, terminata l'assegnazione di variabili per i dettagli nel bottone Genera. Da completare i valore casuali di carrozza e codice cp

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,75 @@
-var btnGenera = document.getElementById('btn_genera');
-var btnAnnulla= document.getElementById('btn_annulla');
+var btnGenera = document.getElementById('btn-genera');
+var btnAnnulla = document.getElementById('btn-annulla');
 
 
-btnGenera.addEventListner("click", function (){
+btnGenera.addEventListener('click', function() {
+
   console.log("genera biglietto");
 
+  var userName = document.getElementById('full-name').value;
+  var km = document.getElementById('km').value;
+  var userAge = document.getElementById('fascia').value;
 
-  var fullNameInput = document.getElementById('full-name');
-  var kmInput = document.getElementById('km');
-  var fasciaInput = document.getElementById('fascia');
+  // var userName = userNameInput.value;
+  // var km = kmInput.value;
+  // var userAge = userAgeInput.value;
 
-  var fullName = fullNameInput.value;
-  var km = fullNameInput.value;
-  var fascia = fullNameInput.value;
+  console.log(userName, km, userAge);
+
+  var kmPrice = km * 0.21;
+  var discount = 0;
+  var offer;
+  console.log(kmPrice, discount);
+
+  if (userAge == "minorenne") {
+   discount = 20/100 * kmPrice;
+   offer = "Sconto Under 18";
+ } else if (userAge == "over65") {
+   discount = 40/100 * kmPrice
+   offer = "Sconto over 65";
+ } else {
+   offer = "Nessuna offerta";
+ }
+
+  console.log(discount, offer);
+
+  var elName = document.getElementById('ticket-userName');
+  var elDiscount = document.getElementById('ticket-offer');
+  var elCarrozza = document.getElementById('ticket-carrozza');
+  var elCp = document.getElementById('ticket-cp');
+  var elTicket = document.getElementById('ticket-cost');
+
+  elName.innerHTML = userName;
+  elDiscount.innerHTML = offer;
+  elCarrozza.innerHTML = km;
+  elCp.innerHTML = km;
+  elTicket.innerHTML = kmPrice - discount;
 
 
-  // CLACOLO
-  var costoPerKm = 0.21;
-  var ticketPrice = costoPerKm * km;
-
-  if (fascia == "minorenne") {
-
-  } else if (fascia == "over65") {
-
-  }
 
 
-})
+
+
+
+
+
+}
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// feaf
